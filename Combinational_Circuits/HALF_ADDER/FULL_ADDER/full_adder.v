281d9e2477f85c259a97814dda07cfd756efad3a
Aditya Pabboju <pabbojuadithya2331@gmail.com>
2026-07-23 00:15:51 +0530

    Delete Combinational_Circuits/HALF_ADDER directory

--- Combinational_Circuits/HALF_ADDER/FULL_ADDER/full_adder.v
+++
@@ -1,7 +0,0 @@
-module full_adder(input A,B,Cin,
-                  output S,C0);
-
-                  assign S = A ^ B ^ Cin;
-                   assign C0 = A & B | B & Cin | Cin & A;
-
-                   endmodule
