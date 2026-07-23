513ee65d57ac26d2237050fcec0cd276218764fe
Aditya Pabboju <pabbojuadithya2331@gmail.com>
2026-07-23 10:25:12 +0530

    Create 4BIT_UpDown_Counter.v

---
+++ Sequential_Circuits/COUNTERS/4BIT_UPDOWN_COUNTER/4BIT_UpDown_Counter.v
@@ -0,0 +1,20 @@
+module updown_counter_4bit(input clk,
+                           input reset,
+                           input up_down,
+                           output reg[3:0]count);
+
+   always @(posedge clk)
+        begin
+
+            if(reset)
+               count <= 4'b0000;
+
+            else if(up_down)
+                count <= count + 1'b1;
+
+              else
+                count <= count - 1'b1;
+
+        end
+
+endmodule  
