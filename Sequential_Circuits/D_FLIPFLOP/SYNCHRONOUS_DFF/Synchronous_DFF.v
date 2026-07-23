64982e36ef9b776805cb6287e4726a9b499d2e69
Aditya Pabboju <pabbojuadithya2331@gmail.com>
2026-07-23 10:11:08 +0530

    Create Synchronous_DFF.v

---
+++ Sequential_Circuits/D_FLIPFLOP/SYNCHRONOUS_DFF/Synchronous_DFF.v
@@ -0,0 +1,15 @@
+module sync_dff(input d,
+                input clk,
+                input reset,       //active high reset
+                output reg q
+                  );
+
+       always @(posedge clk) begin
+          if(reset) 
+             q <= 0;
+
+         else
+           q <= d;     
+       end
+
+endmodule   
